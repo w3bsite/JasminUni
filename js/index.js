5659fb9cf46be6f1a0cef12c759e132e04c24e5c
albatraozh <81822900+albatraozh@users.noreply.github.com>
2022-01-15 10:29:29 +0330

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 var postObjects = [];
 
 document.querySelector("#app").innerHTML = "";
-// const url = "https://jsonplaceholder.typicode.com";
+const url = "https://jsonplaceholder.typicode.com";
 const row = document.querySelector("#row");
-var postList = postObjects;
 if (row) row.innerHTML = `<h2>loading</h2>`;
 var card = (title, content, article) => {
   return `        <div class="card col-md-3">
@@ -21,7 +20,7 @@ var card = (title, content, article) => {
 const loadmore = document.querySelector("#loadmore");
 loadmore.innerHTML = `<div class="row justify-content-center" id='loadmore'>
 <button onclick="load(4)&buttom()" id="butt" type="button" class="btn color">
-<i class="bi bi-caret-down"></i>Load More</button>
+<i class="bi bi-caret-down "></i class='btn btn-primary'> Load More</button>
 </div>`;
 
 function buttom() {
@@ -34,28 +33,42 @@ function buttom() {
 }
 
 let i = 4;
-// function load(x) {
-//   row.innerHTML = ``;
-//   x ? (i = i + x) : (i = i + 0);
-//   fetch(url + "/posts")
-//     .then((response) => response.json())
-//     .then((res) => {
-//       res.slice(0, i).forEach((element) => {
-//         row.innerHTML += card(element.title, element.body);
-//       });
-//     });
-// }
-
+let rand = 5;
+rand = Math.floor(Math.random() * 11);
 function load(x) {
   row.innerHTML = ``;
   x ? (i = i + x) : (i = i + 0);
-  postList.slice(0, i).forEach((element, i) => {
-    row.innerHTML += card(element.title, element.content, i);
-  });
+  fetch(url + "/posts")
+    .then((r) => r.json())
+    .then((res) => {
+      jumbo(res[rand]);
+      res.slice(0, i).forEach((element, i) => {
+        row.innerHTML += card(element.title, element.body, i);
+      });
+    });
 }
+
+// function load(x) {
+//   row.innerHTML = ``;
+//   x ? (i = i + x) : (i = i + 0);
+//   postList.slice(0, i).forEach((element, i) => {
+//     row.innerHTML += card(element.title, element.content, i);
+//   });
+// }
 
 (async function () {
   await load(0);
 })();
 window.load = load;
 window.buttom = buttom;
+function jumbo(j) {
+  document.querySelector("#jumbo").innerHTML = `  
+      <h3 class="display-4">${j.title.slice(0, 10)}</h3>
+  <p>
+  ${j.title.slice(0, 100)}
+  </p>
+  <hr class="my-4" />
+  <p>
+  <a href="/post.html?article=${rand}" class="btn btnCard">Go somewhere</a>
+  </p>`;
+}
